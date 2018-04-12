daa7a8a9756327c849c1c10f9679d10a446163c2
yvesalainbeh <yves-alain.beh-ext@vc-partner.net>
2018-04-12 17:52:53 +0200

    SGP-1062_RexNodes : more comments in RSTimesManager
    Former-commit-id: 32defe8feeea00283f4dfa981521b4160284ccca

--- src/RSTimesManager.h
+++ src/RSTimesManager.h
@@ -37,18 +37,23 @@ public:
      */
     virtual qint64 getStepDate() const;
 
-    //!brief : assign the maximum step value
-    void setStepViewValue(int data);
+    //!brief :setStepViewValue assign the maximum step value
+    //! @param stepMax  the step max to assign
+    void setStepViewValue(int stepMax);
 
     //!brief : load last times settings from setting file
     void loadSettings(const QString& fileName) ;
 
-    //!brief : save last times settings to  setting file
+    /**
+     * @brief : save last times settings to  setting file
+     * @param fileName the path of the settings
+     */
     void saveSettings(const QString& fileName) const;
 
     //!brief : return the greatest step
     int stepViewMax() const ;
 
+    //!@brief stepDays returns the number of day per step
     int stepDays() const ;
 
 protected:
@@ -56,18 +61,34 @@ protected:
     //! Load the settings and update the interface
     void createObjects();
 
-    //!Connect slots to signals
+    //! Connect slots to signals
     void createConnections();
 
+    //! @brief loadStartDate. return the start date defined by the user. The default is current date minus 5 years
     QVariant loadStartDate();
+
+    //!@brief loadEndDate return the default end date in the settings file. Current date is the default value
     QVariant loadEndDate();
+
+    //!@brief loadStepDate  return the step in the settings. The default value is one week (7 Days)
     QVariant loadStepDate();
 
+    //!@brief saveStartDate add the start date in the settings fir rexSensors.ini
     void saveStartDate()const;
+
+    //!@brief saveEndDate add the end date in the settings fir rexSensors.ini
     void saveEndDate()const;
+
+    //!@brief saveEndDate add the step  in the settings fir rexSensors.ini
     void saveStepDate()const;
 
-    void setDateEditValue(QDateEdit*, QDate& value);
+    /**
+     * @brief setDateEditValue in sert a date in a QDateEdit by blocking signals
+     * @param object pointer on the objectName
+     * @param date the date to set
+     */
+    void setDateEditValue(QDateEdit* object, QDate& date);
+
     void updateStepViewMax(bool emitSignal = true);
 
 public slots:
@@ -82,8 +103,11 @@ public slots:
 
 private:
     Ui::RSTimesManager *ui;
+
     RSDatabaseAccess* m_databaseAccess;
+
     QString m_id;
+
     int m_stepViewMax;
 };
 
